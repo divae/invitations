666ce3d3444095d8262e5a8d949cf4dbe365d118
divae <stlmedrano@gmail.com>
2018-12-13 19:12:03 +0100

    export app and server

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-require('./routes/invitationRoutes')(app);
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
-var server = app.listen(PORT);
+const server = app.listen(PORT);
 
 exports.closeServer = function(){
   server.close();
 };
+
+module.exports = server;
