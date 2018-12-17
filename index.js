b19987f1d5621927a8193753214084d790940393
divae <stlmedrano@gmail.com>
2018-12-17 07:37:13 +0100

    Cambio mensaje de bienvenida

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT);
+
+const server = app.listen(5000, function () {
+  console.log('¡Bienvenido! Esperamos que la visita merezca la pena');
+
+  console.log('Visita api en http://localhost:5000/');
+});
 
 exports.closeServer = function(){
   server.close();
